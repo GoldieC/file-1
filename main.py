9b7291fbb4a7bd2238e4f639cf5f148cb79adc0f
GoldieC <mcarnahan11@ivytech.edu>
2026-02-23 20:19:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Variable c is cats
+c = "cats"
+
+# Variable d is dogs
+d = "dogs"
+
+# Variable s is the complete sentence
+s = "It is raining " + c + " and " + d + "."
+
+# Print the sentence
+print(s)
